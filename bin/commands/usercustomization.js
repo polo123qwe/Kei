0b74bd9c3ae9702bb24f16e9ffdc1fae4536a9dc
Sergi B <sergibernaus@yahoo.es>
2016-10-24 02:54:32 +0300

    Fixed (maybe) path for color (line 145)

--- bin/commands/usercustomization.js
+++ bin/commands/usercustomization.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var Command = require('../commandTemplate');
 var Connection = require('../dbConnection');
 var levels = require('../../consts/levels.json');
@@ -7,9 +8,9 @@ var dbUtils = require('../dbUtils');
 var discordUtils = require('../discordUtils');
 var commands = [];
 
-try{
+try {
     var colors = require('../../consts/values.json').colors;
-} catch(e){
+} catch (e) {
     console.log("Error loading colors");
     colors = [];
 }
@@ -26,29 +27,33 @@ cmd.execution = function(client, msg, suffix) {
 
     //Roles are sepparated by commas
     var elements = suffix.join(" ").split(/ ?, ?/);
-    for(var elem of elements){
+    for (var elem of elements) {
         var role = discordUtils.getRole(msg.guild, elem);
-        if(role && rolesFound.indexOf(role) == -1){
+        if (role && rolesFound.indexOf(role) == -1) {
             rolesFound.push(role);
         }
     }
 
     dbUtils.fetchGuild(msg.guild.id, function(err, guildData) {
-        if(err) return utils.sendAndDelete(msg.channel, err);
-        if(guildData.hasOwnProperty('roles')){
-            for(var roleID of guildData.roles){
-                var role = rolesFound.find((r) => {return r.id == roleID});
-                if(role){
+        if (err) return utils.sendAndDelete(msg.channel, err);
+        if (guildData.hasOwnProperty('roles')) {
+            for (var roleID of guildData.roles) {
+                var role = rolesFound.find((r) => {
+                    return r.id == roleID
+                });
+                if (role) {
                     rolesToAdd.push(role);
                 }
             }
             //If no role was found, print out all the possibilities for the user to choose
-            if(rolesToAdd.length < 1){
+            if (rolesToAdd.length < 1) {
                 var out = "Error, choose one of the following: ";
                 var possibleRoles = [];
-                for(var roleID of guildData.roles){
-                    var role = msg.guild.roles.find((r) => {return r.id == roleID});
-                    if(role){
+                for (var roleID of guildData.roles) {
+                    var role = msg.guild.roles.find((r) => {
+                        return r.id == roleID
+                    });
+                    if (role) {
                         possibleRoles.push(role.name);
                     }
                 }
@@ -75,24 +80,26 @@ cmd.execution = function(client, msg, suffix) {
 
     //Roles are sepparated by commas
     var elements = suffix.join(" ").split(/ ?, ?/);
-    for(var elem of elements){
+    for (var elem of elements) {
         var role = discordUtils.getRole(msg.guild, elem);
-        if(role && rolesFound.indexOf(role) == -1){
+        if (role && rolesFound.indexOf(role) == -1) {
             rolesFound.push(role);
         }
     }
 
     dbUtils.fetchGuild(msg.guild.id, function(err, guildData) {
-        if(err) return utils.sendAndDelete(msg.channel, err);
-        if(guildData.roles){
-            for(var roleID of guildData.roles){
-                var role = rolesFound.find((r) => {return r.id == roleID});
-                if(role){
+        if (err) return utils.sendAndDelete(msg.channel, err);
+        if (guildData.roles) {
+            for (var roleID of guildData.roles) {
+                var role = rolesFound.find((r) => {
+                    return r.id == roleID
+                });
+                if (role) {
                     rolesToRemove.push(role);
                 }
             }
         }
-        if(rolesToRemove.length < 1) return utils.sendAndDelete(msg.channel, "Nothing to remove!");
+        if (rolesToRemove.length < 1) return utils.sendAndDelete(msg.channel, "Nothing to remove!");
         msg.member.removeRoles(rolesToRemove).then((memb) => {
             msg.channel.sendMessage(msg.author.username + " removed successfully!");
         }).catch(err => utils.sendAndDelete(msg.channel, ':warning: Bot error! ' + err.response.body.message));
@@ -110,37 +117,37 @@ cmd.execution = function(client, msg, suffix) {
 
     var value, index;
     //If its a hex input
-    if(/^#?[0-9A-F]{6}$/.test(suffix[0])){
+    if (/^#?[0-9A-F]{6}$/.test(suffix[0])) {
         value = suffix[0];
         //We remove the # if it starts with it
-        if(value.startsWith("#")) value = value.substr(1, value.length);
-    } else if (/^\d{1,2}$/.test(suffix[0])){ //If its a value to get from the index of values
+        if (value.startsWith("#")) value = value.substr(1, value.length);
+    } else if (/^\d{1,2}$/.test(suffix[0])) { //If its a value to get from the index of values
         index = suffix[0];
     }
 
-    dbUtils.fetchGuild(msg.guild.id, function(err, guildData){
-        if(err) return utils.sendAndDelete(msg.channel, err);
-        if(guildData.hasOwnProperty('limitedcolors') && guildData.limitedcolors){
+    dbUtils.fetchGuild(msg.guild.id, function(err, guildData) {
+        if (err) return utils.sendAndDelete(msg.channel, err);
+        if (guildData.hasOwnProperty('limitedcolors') && guildData.limitedcolors) {
             //Limited colors
-            if(suffix[0]){
-                if(index){
-                    if(index > 0 && index <= colors.length){
-                        return setupColor(colors[index-1]);
+            if (suffix[0]) {
+                if (index) {
+                    if (index > 0 && index <= colors.length) {
+                        return setupColor(colors[index - 1]);
                     }
                 }
-                if(value){
-                    if(colors.includes(value)){
+                if (value) {
+                    if (colors.includes(value)) {
                         return setupColor(value);
                     }
                 }
             }
 
-            msg.channel.sendFile('./consts/colors.png', 'colors.png', 'Error, the colors available are:');
+            msg.channel.sendFile(path.resolve(__dirname, 'consts/colors.png'), 'colors.png', 'Error, the colors available are:');
             return;
 
         } else {
             //Not limited
-            if(suffix[0] && value){
+            if (suffix[0] && value) {
                 return setupColor(value);
             } else {
                 msg.channel.sendMessage('Write a hexadecimal value! eg: #FFFFFF');
@@ -150,7 +157,7 @@ cmd.execution = function(client, msg, suffix) {
     });
 
     //We remove the old colors the user had and apply the new one
-    function setupColor(name){
+    function setupColor(name) {
 
         removeExistingRoles(() => {
             setRole(name);
@@ -158,20 +165,20 @@ cmd.execution = function(client, msg, suffix) {
     }
 
     //Function that removes the roles from the user
-    function removeExistingRoles(callback){
+    function removeExistingRoles(callback) {
         /*
          * We remove the role from the user and check if any other
          * user is also on that role, if not we remove the role itself
          */
         var role = msg.member.roles.find(r => r.name.startsWith("#"));
         //If we don't find more roles to remove, finish the function
-        if(!role){
+        if (!role) {
             return callback();
         }
 
         msg.member.removeRole(role).then(() => {
             //If role is empty, remove it
-            if(role.members.array().length < 1){
+            if (role.members.array().length < 1) {
                 role.delete().then(() => {
                     return removeExistingRoles(callback);
                 }).catch(console.log);
@@ -182,13 +189,13 @@ cmd.execution = function(client, msg, suffix) {
     }
 
     //Function to set the current role
-    function setRole(name){
+    function setRole(name) {
         // We try to find the role and if we cannot find it we create it
         var role = msg.guild.roles.find(r => {
             return r.name == '#' + name;
         });
 
-        if(!role){
+        if (!role) {
             var options = {
                 name: '#' + name,
                 color: parseInt(name, '16'),
@@ -201,7 +208,7 @@ cmd.execution = function(client, msg, suffix) {
             addUser(role);
         }
 
-        function addUser(roleToAdd){
+        function addUser(roleToAdd) {
             msg.member.addRole(roleToAdd).then(() => {
                 msg.channel.sendMessage(msg.author.username + ' successfully added to #' + name);
             }).catch(console.log);
